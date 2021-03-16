176af91f55307a59788cfcc2bd52aaa91f4de96d
Alexandra Lukinicheva <a.lukinicheva@gmail.com>
2021-03-16 14:16:01 -0700

    start react components for eventonica

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+//import headerImg from './'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} className="jumbotron" alt="header-img" />
+        <h1>
+          Eventonica 
+        </h1>
+        <p>Event Recommender</p>
+        <nav>
+          <ul>
+            <li>Sign in</li>
+            <li>Login</li>
+          </ul>
+        </nav>
       </header>
+      <main>
+        <h2>Events</h2>
+        <section>
+          <ul>
+            <li></li>
+          </ul>
+        </section>
+      </main>
     </div>
   );
 }
---
+++ src/Eventonica.js
@@ -0,0 +1,146 @@
+{/* NAVIGATION */}
+function Nav() {
+  return (
+    <nav>
+      <ul>
+        {/* if logged in/user_id is defined */}
+        <li>Sign in</li>
+        <li>Login</li>
+        {/* else */}
+        {/*
+        <li>Profile</li>
+        <li>Add event</li>
+        <li>Favorites</li>s
+        */}
+      </ul>
+    </nav>
+  );
+}
+{/* HEADER */}
+function Header() {
+  return (
+    <header className="App-header">
+      <img src={/* headerImg */} className="jumbotron" alt="header-img" />
+      <h1>
+        Eventonica 
+      </h1>
+      <p>Event Recommender</p>
+      <Nav></Nav>
+    </header>
+  );
+}
+{/* MAIN */}
+{/* Events or Search Results */}
+function Main({title}) {
+  return (
+    <main>
+      <h2>{title}</h2>
+      <section>
+        <ul>
+          {/* dynamically create lis from Eventonica database 
+          Events table */}
+          <li></li>
+        </ul>
+      </section>
+    </main>
+  );
+}
+{/* ADD EVENT FORM */}
+function AddEventForm() {
+  return (
+    <>
+    <h3>Add Event</h3>
+    <form id="add-event" action="/addEvent" method="POST">
+      <fieldset>
+        <label>Name:</label>
+        <input
+          type="text" name="name"
+          id="add-event-name"
+          placeholder="Event Name"
+        />
+        <label for="event-category">Category:</label>
+        <input
+         type="text" name="category"
+         id="event-category" 
+         placeholder="Event Category"
+        />
+
+        <label for="event-location">Location:</label>
+        <input
+         type="text" name="location"
+         id="event-location" 
+         placeholder="Location"
+        />
+
+        <label for="event-date">Date:</label>
+        <input
+         type="text" name="date"
+         id="event-date" 
+         placeholder="MM-DD-YYYY"
+        />
+      
+        <label for="event-time">Time:</label>
+        <input
+         type="text" name="time"
+         id="event-time" 
+         placeholder="HH-MM"
+        />
+
+        <label for="event-price">Price:</label>
+        <input
+         type="text" name="price"
+         id="event-price" 
+         placeholder="0.00"
+        />
+      </fieldset>
+      <input type="submit" />
+      </form>
+      </>
+  );
+}
+{/* USER SIGN IN FORM */}
+function AddUser() {
+  return (
+    <div>
+      <h3>Sign In:</h3>
+      <form id="add-user" action="/addUser" method="POST">
+        <fieldset>
+          <label>Username:</label>
+          <input type="text" id="add-username" name="username" />
+        </fieldset>
+        <fieldset>
+          <label for="fname">First name:</label>
+          <input type="text" id="fname" name="fname" />
+        </fieldset>
+        <fieldset>
+          <label for="lname">Last name:</label>
+          <input type="text" id="lname" name="lname" />
+        </fieldset>
+        <fieldset>
+          <label for="email">Email:</label>
+          <input type="email" id="email" name="email" />
+        </fieldset>
+        <input type="submit" value="Signup" />
+      </form>
+    </div>
+  );
+}
+{/* USER LOGIN FORM */}
+function Login() {
+  return (
+    <div>
+      <h3>Login</h3>
+      <form id="add-user" action="/addUser" method="POST">
+        <fieldset>
+          <label>Username:</label>
+          <input type="text" id="add-username" name="username" />
+        </fieldset>
+        <fieldset>
+          <label for="email">Email:</label>
+          <input type="email" id="email" name="email" />
+        </fieldset>
+        <input type="submit" value="Login" />
+      </form>
+    </div>
+  );
+}
